ab719147571493a7785d17772da95a3fec58bd5d
akshayshenoy <akshayshenoy@10.0.0.148>
2018-08-05 16:13:08 -0700

    test

--- algoconcepts/src/main/java/com/algoconcepts/backtracking/combinationsforstring/Solution.java
+++ algoconcepts/src/main/java/com/algoconcepts/backtracking/combinationsforstring/Solution.java
@@ -1,9 +1,33 @@
 package com.algoconcepts.backtracking.combinationsforstring;
 
+import java.util.ArrayList;
+
 public class Solution {
 
+	public void computeC(String s) {
+		ArrayList<String> sols = new ArrayList<String>();
+		helperC(s, "", sols);
+	}
+	
+	public void helperC(String s, String cs, ArrayList<String> sols) {
+		if(s.isEmpty()) {
+			sols.add(cs);
+		}else {
+			for(int i=0;i<s.length();i++) {
+				char ch = s.charAt(i);
+				cs = cs + ch;
+				s = s.substring(0, i) + s.substring(i+1, s.length()-1);
+				helperC(s,cs,sols);
+				
+			}
+		}
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Solution s1 = new Solution();
+		String s = "ABCD";
+		s1.computeC(s);
 
 	}
 
